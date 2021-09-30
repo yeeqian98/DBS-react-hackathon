66704b17ac9ae18707792677a88e81778267b9d8
najib333 <najibbijan95@gmail.com>
2021-09-30 13:16:45 +0800

    loginauthentication update

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,13 @@
-from flask import Flask, render_template, request
+from flask import Flask, Blueprint, request
+from flask_login.utils import login_user
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import current_user, login_user, LoginManager
+from flask_login import UserMixin, LoginManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
-login_manager = LoginManager()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True) 
@@ -19,15 +19,15 @@ class User(db.Model):
     gender = db.Column(db.String(6))
     created_at = db.Column(db.DateTime)
 
-    def __repr__(self):
-        return f"User('{self.username}')"
-
     def check_password(self, password):
         """Check hashed password."""
         return check_password_hash(self.password, password)
 
     def set_password(self,password):
         self.password = generate_password_hash(password)
+
+    def __repr__(self):
+        return f"User('{self.username}')"
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key = True) 
@@ -49,19 +49,22 @@ class Category(db.Model):
 def members():
     return {'members': ["1","2","3"]}
 
-@app.route("/api/login", methods=['POST'])
+@auth.route("/api/login", methods=['POST'])
 def login():
-    if current_user.is_authenticated:
-        return redirect('/home')
-
     if request.method == 'POST':
-        username = request.form['username']
-        user = UserModel.query.filter_by(username = username).first()
-        if user is not None and user.check_password(request.form['password']):
-            login_user(user)
-            return {
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = User.query.filter_by(username = username).first()
+        if not user or not check_password_hash(user.password, password):
+            return{
+                'statusCode': 400,
+                'message' : 'Invalid Login'
+            }
+
+        login_user(user)
+        return{
                 'statusCode': 200,
-                'username' : username
+                'message' : username
             }
 
 if __name__ == '__main__':
