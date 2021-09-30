4d32e55f4827b705f96947a3af64104b512a63fe
Mok Kai Sheng Daryl <54903425+DeetoMok@users.noreply.github.com>
2021-09-30 11:23:33 +0800

    added db models

--- backend/app.py
+++ backend/app.py
@@ -9,24 +9,44 @@ db = SQLAlchemy(app)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True) 
     username = db.Column(db.String(20), unique=True, nullable=False) 
-    email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
+    first_name = db.Column(db.String(20))
+    last_name = db.Column(db.String(20))
+    postal_code = db.Column(db.Integer)
+    gender = db.Column(db.String(6))
+    created_at = db.Column(db.DateTime)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}')"
 
-# class 
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key = True) 
+    title = db.Column(db.String(20))
+    price = db.Column(db.Integer)
+    description = db.Column(db.String(20))
+    category_id = db.Column(db.String(20))
+    image = db.Column(db.String(20))
+    qty = db.Column(db.Integer)
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key = True) 
+    name = db.Column(db.String(20))
+    description = db.Column(db.String(20))
+    image = db.Column(db.String(20))
 
 
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    return render_template('index.html')
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+#     pass
+#     pass
+#     return render_template('index.html')
 
 @app.route("/api/members")
 def members():
     return {'members': ["1","2","3"]}
 
+# @app.route*"/api/login")
 
 if __name__ == '__main__':
     app.run(debug=True)
